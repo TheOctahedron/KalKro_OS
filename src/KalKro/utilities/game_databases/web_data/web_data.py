940c5bdbacec4c6a11861471c2287e19b55bf31a
Octahedron <octahedron929@gmail.com>
2026-07-29 22:37:18 +0300

    feat: added embedded-Internet vars folder
    A folder and a file containing popular web queries have been added.
    - (web_data/)  folder containing files with vars for the embedded web.
    - (web_data/web_data.py)  A file containing var with popular queries on the web (popular_questions).

---
+++ src/KalKro/utilities/game_databases/web_data/web_data.py
@@ -0,0 +1,8 @@
+popular_questions = {
+  1: "Interesting facts about birds",
+  2: "Download the 'Rock paper scissors'",
+  3: "Download the marketing simulator (Beta)",
+  4: "Information about the developer",
+  5: "Download DojDo Ai",
+  6: "FIRE: WRITE YOUR OWN API"
+}
