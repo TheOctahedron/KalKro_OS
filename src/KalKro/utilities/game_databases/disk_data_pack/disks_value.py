9a691713ce6f8881ef4e4ca40e79a9325592a3cc
Octahedron <octahedron929@gmail.com>
2026-07-30 23:42:30 +0300

    feat: added folder with vars for disk-data
    A folder and a files (2) for storing the disk-date have been added.
    - (disk_data_pack/)  File storage folder with vars for disks.
    - (disk_data_pack/disks_value.py)  A file with the variable 'disks_value', which stores information about the size of each disk.
    
    - (disk_data_pack/__init__.py)  __init__ file, for pyproject.toml

---
+++ src/KalKro/utilities/game_databases/disk_data_pack/disks_value.py
@@ -0,0 +1,5 @@
+disks_value = [
+  {"id": 1, "type": "DISK", "memory": 1000, "content": []},
+  {"id": 2, "type": "DISK", "memory": 1000, "content": []}, 
+  {"id": 3, "type": "DISK", "memory": 1000, "content": []}
+]
